6bc77ca15573f5e450ed834b3891853fd466c475
James Tran <jv.fuisl@gmail.com>
2023-07-02 18:15:04 +0700

    implemented folder initialization

--- qrgen.py
+++ qrgen.py
@@ -1,6 +1,14 @@
 import os
+from qrcode import QRCode
 
-import qrcode
+def gen(codes:list, folder_path='./qrcodes/'):
+    folder_path = './'+folder_path+'/' if not(os.path.isdir(folder_path)) else folder_path
+    try:
+        os.mkdir(folder_path)
+        print(f"Folder '{folder_path}' created successfully.")
+    except:
+        pass
 
-def gen():
-    raise NotImplementedError
+    
+
+gen()
