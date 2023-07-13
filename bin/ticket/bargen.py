de2ef8c745b8701eb6c0bb8ad85de9e065a1177c
James Tran <jv.fuisl@gmail.com>
2023-07-13 14:12:53 +0700

    fix: fix bug in destinated generate folder
    -

--- bin/ticket/bargen.py
+++ bin/ticket/bargen.py
@@ -13,7 +13,7 @@ def gen(codes:list, folder_path='./barcodes/', transparent=False):
 
     (optional) Transparent barcode
     """
-    folder_path = './'+folder_path+'/' if not(os.path.isdir(folder_path)) else folder_path
+    folder_path = './'+folder_path+'/barcodes' if not(os.path.isdir(folder_path)) else folder_path
     try:
         os.mkdir(folder_path)
         print(f"Folder '{folder_path}' created successfully.")
