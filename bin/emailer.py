05c5ebf3e97cc73057a4c1a8c1cb7f6f14547fa8
fuisl <135719107+fuisl@users.noreply.github.com>
2024-03-24 13:54:04 +0700

    fix: update emailer to send in bulk.

--- bin/emailer.py
+++ bin/emailer.py
@@ -1,57 +1,104 @@
 import smtplib
 
-from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
 
-"""
-Bulk emailer for sending emails to students.
+class Emailer:
+    '''
+    Emailer class for sending tickets.
+    '''
+    def __init__(self, server="smtp.gmail.com", port=587):
+        self.server = smtplib.SMTP(server, port)
+        self.server.ehlo()
+        self.server.starttls()
+        self.login_status = False
 
-FIXME: Cannot bulk sending --> wrong methods, take longs to send --> queueing issue.
-"""
+    def login(self, email, password):
+        """
+        Login to the email server. Must be called before sending email.
 
-def send_email(info: dict):
-    msg = MIMEMultipart()
-    msg["From"] = info["from"]
-    msg["To"] = info["to"]
-    msg["Subject"] = info["subject"]
+        :param email: str --> email address
+        :param password: str --> password
+        """
+        try:
+            self.from_email = email
+            self.server.login(email, password)
+            self.login_status = True
+            self.server.sendmail(self.from_email, "10422021@student.vgu.edu.vn", "Server is ready to send email.")
+        
+        except smtplib.SMTPResponseException as e:
+            print("smtplib.SMTPResponseException: " + str(e.smtp_code) + " " + str(e.smtp_error))
 
-    template = info["template"]
-    code = info["code"]
+    def send(self, info: dict):
+        if not self.login_status:
+            raise Exception("You must login to use this feature!")
+        
+        msg = self.content(info)
 
-    with open(f"./template/ied23.html") as template: # TODO: Replace with information from the database
-        body = template.read().replace("[code]", info['code'])
-        body = body.replace("[name]", info['name'])
-        body = body.replace("(#000000)", '(' + info['number'] + ')')
-        msg.attach(MIMEText(body, "html"))
+        try:
+            self.server.sendmail(self.from_email, info["to"], msg)
+        except smtplib.SMTPResponseException as e:
+            print("smtplib.SMTPResponseException: " + str(e.smtp_code) + " " + str(e.smtp_error))
+            return 1
+        
+    def send(self, infos: list):
+        """
+        Send email in bulks.
 
-    with open(f"./qrcodes/{code}.png", "rb") as attachment:
-        img = MIMEImage(attachment.read())
-        img.add_header('Content-ID', 'qr')
-        msg.attach(img)
+        :param infos: list --> list of dict, dict format in content
+        """
+        if not self.login_status:
+            raise Exception("You must login to use this feature!")
+        
+        for info in infos:
+            """
+            for each ticket, send email.
+            """
+            self.send(info)
 
+        return 0  # Success
 
-    text = msg.as_string()
+    @staticmethod
+    def content(self, info: dict):
+        """
+        Return content as string for sending email.
 
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.ehlo()
-    server.starttls()
-    server.login(info['from'], info['key'])
-    server.sendmail(info['from'], info['to'], text)
-    server.quit()
+        :param info: dict --> information of the ticket
+            - subject: str
+            - to: str
+            - name: str
+            - code: str
+            - ...: str
+        """
+        msg = MIMEMultipart()
+        msg['Subject'] = info['subject']
+        msg['From'] = self.from_email
+        msg['To'] = info['to']
 
+        template = ""  # path to template
 
+        with open(template, 'r') as file:
+            """
+            Read the template file and replace all place holder with values.
+            """
+            content = file.read()
+            content = content.replace("[code]", "codehere")
+            content = content.replace("[name]", "namehere")
+            content = content.replace("(#000000)", '(' + "#000000" + ')')
+            # Replace all place holder with values. Why don't just use f"{}"? --> more control over the content
+            msg.attach(MIMEText(content, 'html'))
 
-if __name__ == "__main__":
-    info = {
-    "from": "presto@vgu.edu.vn",
-    "to":"10422021@student.vgu.edu.vn",
-    "subject": "[IED23] Entrance Ticket",
-    "template": "ied23.html",
-    "name": "CSE2022",
-    "number": "#CSE2022",
-    "code": "CSE2022",
-    "key": ""
-    }
+        with open("path/to/QRCode", 'rb') as file:
+            """
+            Attach QR code to the email and place to the content.
+            """
+            img = MIMEImage(file.read())
+            img.add_header('Content-ID', 'qr')  # Content-ID is used to refer to the image in the HTML content --> <img src="cid:qr">
+            msg.attach(img)
 
-    send_email(info)
+        return msg.as_string()
+    
+    def __del__(self):
+        self.server.quit()
+        self.login_status = False
